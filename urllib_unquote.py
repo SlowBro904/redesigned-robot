7df79c10b701e049304fec6ec943e2a3181aee73
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-06-22 08:56:08 -0400

    Add files via upload

---
+++ urllib_unquote.py
@@ -0,0 +1,18 @@
+"""Subset of the urllib I found here. Cut it down to save memory and to fix a stack overflow. Only the unquote module. https://github.com/lucien2k/wipy-urllib/blob/master/urllib.py"""
+
+def unquote(s):
+    """Kindly rewritten by Damien from Micropython"""
+    """No longer uses caching because of memory limitations"""
+    res = s.split('%')
+    for i in range(1, len(res)):
+        item = res[i]
+        try:
+            res[i] = chr(int(item[:2], 16)) + item[2:]
+        except ValueError:
+            res[i] = '%' + item
+    return "".join(res)
+
+def unquote_plus(s):
+    """unquote('%7e/abc+def') -> '~/abc def'"""
+    s = s.replace('+', ' ')
+    return unquote(s)
