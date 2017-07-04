9bbd7987b993c219c3df36b4619b59e1f7e21658
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-07-04 16:08:32 -0400

    Delete buildResponse.py

--- buildResponse.py
+++
@@ -1,8 +0,0 @@
-def buildResponse(title, header, h1, body):
-    """ Takes the values for a web page and builds a complete output """
-    if type(body) is not list:
-        body = [body]
-    
-    body = '\n'.join(body)
-    response = html % (title, header, h1, body)
-    return "HTTP/1.0 200 OK\r\nContent-type: text/html\r\nContent-length: %d\r\n\r\n%s" % (len(response), response)
