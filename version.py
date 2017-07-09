955cd261f5c82184a7335dbb6f6820860eb13eb8
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-07-09 18:47:00 -0400

    Delete version.py

--- version.py
+++
@@ -1,6 +0,0 @@
-"""Sets the version number variable based on the version number file"""
-from config import config
-from maintenance import maintenance
-
-maintenance()
-version = open(config['VERSION_NUMBER_FILE']).readlines().strip()
