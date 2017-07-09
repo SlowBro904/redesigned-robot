803c67efc7b1275dfabca253dcbdd9003ea97594
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-07-09 18:49:14 -0400

    Delete serial.py

--- serial.py
+++
@@ -1,7 +0,0 @@
-""" Sets our serial number variable based on the system's unique ID """
-from binascii import hexlify
-from machine import unique_id
-from maintenance import maintenance
-
-maintenance()
-serial = str(hexlify(unique_id()), 'utf-8')
