5a96886ac718d60b4e9f6398e2f54edf4a93ba25
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-07-09 19:15:29 -0400

    Delete status.py

--- device_routines/door_reed_switches/status/status.py
+++
@@ -1,6 +0,0 @@
-""" Sets variables for the door reed switch status. Basically this tells if the reed switches are active, and whether the door is in the up or down position. """
-from machine import Pin
-from config import config
-
-up = Pin('P' + config['DOOR_REED_UP_PIN'], mode = Pin.IN, pull = Pin.PULL_UP)
-dn = Pin('P' + config['DOOR_REED_DN_PIN'], mode = Pin.IN, pull = Pin.PULL_UP)
