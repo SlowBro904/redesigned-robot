7585da378b5996d65784cf8138c0cba4d6d5d5bb
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-08-17 21:59:47 -0400

    Add files via upload

--- process_mqtt.py
+++ process_mqtt.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 # From http://www.steves-internet-guide.com/into-mqtt-python-client/
-from os import listdir
-from json import dumps
+import os
+from glob import glob
 from time import sleep
-from hashlib import sha512
+from haslib import sha512
+from itertools import chain
 from re import sub as re_sub
 import paho.mqtt.client as mqtt
+from json import dumps, load, dump
 #from crypto import AES, getrandbits
 
 debug_enabled = True
 default_level = 0
-
-# Locations for client code
-# FIXME Reconcile (maybe?) client version to code version?
-# FIXME Do I want to just do /SB/current?
-client_code = {'SB': '/SB/0.0.0'}
-
 def debug(msg, level = 0):
     '''Prints a debug message'''
     if not debug_enabled:
@@ -34,39 +30,68 @@ def on_message(client, userdata, in_msg):
     debug("type(in_msg.topic): '" + str(type(in_msg.topic)) + "'", level = 0)
     
     # Just the end subtopic, the rest is meta
-    device_type = in_msg.topic.split('/')[0]
     topic = in_msg.topic.split('/')[-1]
 
     if topic == 'ping':
         # Don't encrypt ping/ack
         out_msg = 'ack'
-    elif topic == 'get_new_dirs':
-        # FIXME Change this to a directory reconcile on clients. Add what's new
-        # and remove what's old.
-        # FIXME Encrypt -- maybe :-)
+    # FIXME Need a list of directories or maybe a protection file
+    # (do_not_purge.json?) that don't get purged when we do an update. Data
+    # directories, config files, and the like.
+    elif topic == 'get_file_list':
         out_msg = ['deleteme']
-    elif topic == 'get_sys_updates':
-        # FIXME And delete files off the local client as well
-        root_path = client_code[device_type]
-        sha_sum = sha512()
-
-        # FIXME Glob subdirectories and find files everywhere
-        for file in listdir(root_path):
-            with open(file) as f:
-                for chunk in iter(lambda: f.read(4096), b""):
-                    sha_sum.update(chunk)
-                
-                sha_sum = sha_sum.digest()
-                f.seek(0)
-            
-                # TODO Can I get the SHA sum AND read the contents at the same
-                # time?
-                contents = f.readlines()
-            
-            out_msg.append([file, sha_sum, contents])
         
     out_topic = re_sub('/in/', '/out/', in_msg.topic)
     client.publish(out_topic, dumps(out_msg))
+
+
+def get_sha_sums(dir):
+    '''Recursively searches a directory for all folders and files and returns a
+    tuple of two lists: The first list is a list of directories and the second 
+    is a list of tuples which are file names with their SHA-512 hash.
+    '''
+    directories = list()
+    files = list()
+    # TODO There's probably a way to combine these statements but I'm not that
+    # advanced yet.
+    for file in (chain.from_iterable(glob(os.path.join(x[0], '*')) for x in os.walk(dir))):
+        if os.isdir(file):
+            # It's actually a directory. Don't attempt SHA-512.
+            directories.append(file)
+            continue
+        
+        sha = sha512()
+        with open(file) as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                sha.update(chunk)
+            sha = sha.digest()
+            files.append((file, sha))
+    return (directories, files)
+
+
+def check_file_list(dir):
+    '''Checks dir + '/version.json' and compares it to dir + '/file_list.json'.
+    If the version stored in version.json is newer than what is in
+    file_list.json then update the latter with a tuple that is the version
+    number, and the directories and file names with SHA-512 sums from
+    get_sha_sums(). To do an update to push out to clients, we update
+    version.json and all other files in the directory, and this will
+    automatically update the file list.
+    '''
+    # FIXME And if it doesn't exist?
+    with open(dir + '/version.json') as f:
+        code_ver = load(f)
+
+    with open(dir + '/file_list.json') as f:
+        file_list_ver = load(f)[0]
+
+    if file_list_ver != code_ver:
+        directories, files = get_sha_sums(dir)
+
+    # FIXME Exclude file_list.json from SHA checking on the client since it
+    # will differ from the line above to now
+    with open(dir + '/file_list.json', 'w') as f:
+        dump((code_ver, directories, files), f)
 
 
 def on_log(client, userdata, level, buf):
@@ -82,7 +107,6 @@ client = mqtt.Client(client_id = 'better_automations')
 client.connect('localhost')
 
 # Client name and version they are at
-# FIXME Pull from a database
 authorized_devices = {'SB': {'240ac400b1b6': '0.0.0'}}
 device_keys = {'SB': {'240ac400b1b6': 'abcd1234'}}
 topics = {'SB': ['ping', 'get_new_dirs']}
@@ -98,7 +122,6 @@ for device_type in authorized_devices:
         root_path = device_type + '/' + serial + '/' + version + '/in/'
         for topic in topics[device_type]:
             mytopic = root_path + topic
-            # FIXME When getting a message, should I/do I want to auto-sub?
             client.subscribe(mytopic, qos = 1)
 
 while True:
