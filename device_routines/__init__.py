f382ba18f66f254a7f0e7e4f8cf29bf53ef49b8c
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2017-09-04 16:06:50 -0400

    Add files via upload

--- device_routines/__init__.py
+++ device_routines/__init__.py
@@ -6,6 +6,7 @@ class DeviceRoutine(object):
         self.err = Err()
         self.device = device
     
+    
     def run(self, command, arguments = None):
         '''This runs a command for a particular device.'''
         from maintenance import maint
@@ -15,11 +16,11 @@ class DeviceRoutine(object):
         
         maint()
         
-        try:
-            return routine(arguments)
-        except:
-            warning = ("Could not run command " + command + " on device ",
-                        device + " with these arguments: '" + str(arguments),
-                        "' ('device_routines/__init__.py','run')")
-            self.err.warning(warning)
-            return False
+        #try:
+        return routine(arguments)
+        #except:
+        #    warning = ("Could not run command " + command + " on device ",
+        #                device + " with these arguments: '" + str(arguments),
+        #                "' ('device_routines/__init__.py','run')")
+        #    self.err.warning(warning)
+        #    return False
